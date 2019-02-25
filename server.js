fd75adb0b6ac15aafa71653b1475626f2955eff0
da Vinci <brucewilson1103@gmail.com>
2019-02-24 20:53:19 -0500

    prep for deploy

--- server.js
+++ server.js
@@ -27,11 +27,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/SWeather", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/SWeather"
+
+mongoose.connect( MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
-// A GET route for scraping the echoJS website
+// A GET route for scraping the echoJS website article
 app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
   axios.get("http://www.spaceweather.com/").then(function (response) {
